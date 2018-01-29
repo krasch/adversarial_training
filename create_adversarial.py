0c7955df1cfeb37232d6acdde373101fc8973d4a
krasch <github@krasch.io>
2018-01-29 22:37:38 +0100

    Need to cast np.float32 to float for json to work

--- create_adversarial.py
+++ create_adversarial.py
@@ -1,31 +1,75 @@
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten
 from keras.models import load_model
+from keras import backend as K
+import numpy as np
 
 from data import load_mnist
 from util import JSONLogger
 
-
-def make_model():
-    model = Sequential()
-    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28,28,1)))
-    model.add(Conv2D(32, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-    model.add(Flatten())
-    model.add(Dense(64, activation='relu'))
-    model.add(Dropout(0.25))
-    model.add(Dense(10, activation='softmax'))
-
-    model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
-    model.summary()
-
-    return model
+pixel_min = 0.0
+pixel_max = 1.0
 
 
-(X_train, y_train), (X_test, y_test) = load_mnist()
-model = make_model()
-model.fit(X_train, y_train, batch_size=64, epochs=2,
-          validation_data=(X_test, y_test),
-          callbacks=[JSONLogger()], verbose=0)
-# model = load_model("mnist_model.h5")
+def init_mnist_model(pretrained=False):
+    if pretrained:
+        return load_model("mnist_model.h5")
+
+    else:
+        model = Sequential()
+        model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28,28,1)))
+        model.add(Conv2D(32, (3, 3), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+        model.add(Flatten())
+        model.add(Dense(64, activation='relu'))
+        model.add(Dropout(0.25))
+        model.add(Dense(10, activation='softmax'))
+
+        model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
+        model.summary()
+
+        (X_train, y_train), (X_test, y_test) = load_mnist()
+        model.fit(X_train, y_train, batch_size=64, epochs=2,
+                  validation_data=(X_test, y_test),
+                  callbacks=[JSONLogger()], verbose=0)
+
+        return model
+
+
+def init_get_gradient(model):
+    get_gradient_functions = {}
+    for target_class in range(10):
+        cost_function = model.output[0, target_class]
+        gradient_function = K.gradients(cost_function, model.input)[0]
+        get_gradient_functions[target_class] = K.function([model.input, K.learning_phase()], [gradient_function])
+
+    def apply(img, target_class):
+        func = get_gradient_functions[target_class]
+        return func([np.array([img]), 0])[0][0, :]
+
+    return apply
+
+
+def FGSM(original_image, get_gradient, target_class, epsilon):
+    pertubation = np.sign(get_gradient(original_image, target_class))
+    adversarial = original_image + epsilon * pertubation
+    return np.clip(adversarial, pixel_min, pixel_max)
+
+
+def main():
+    model = init_mnist_model(pretrained=False)
+    """
+    get_gradient = init_get_gradient(model)
+
+    _, (X_test, y_test) = load_mnist()
+
+    for i in range(10):
+        adversarial = FGSM(X_test[i], get_gradient, 4, epsilon=0.3)
+
+        scipy.misc.imsave("zebra_adversarial2.jpg", adversarial)
+    """
+
+main()
+
+
--- util.py
+++ util.py
@@ -1,6 +1,14 @@
 import json
 
 from keras.callbacks import Callback
+import numpy as np
+
+
+def dump_json(some_dict):
+    for key in some_dict:
+        if isinstance(some_dict[key], np.float32):
+            some_dict[key] = float(some_dict[key])
+    return json.dumps(some_dict)
 
 
 class JSONLogger(Callback):
@@ -14,12 +22,12 @@ class JSONLogger(Callback):
         logs = {key: value for key, value in logs.items() if key not in ["size", "batch"]}
         logs["samples"] = self.samples_seen
         logs["epoch"] = epoch
-        print(json.dumps(logs))
+        print(dump_json(logs))
 
     def on_batch_end(self, batch, logs=None):
         self.samples_seen += logs["size"]
 
         if batch % 10 == 0:
-            logs = {key:value for key, value in logs.items() if key not in ["size", "batch"]}
+            logs = {key: value for key, value in logs.items() if key not in ["size", "batch"]}
             logs["samples"] = self.samples_seen
-            print(json.dumps(logs))
+            print(dump_json(logs))
