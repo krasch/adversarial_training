42610c22dcdef27f34d1eff6b2bc2b7abf3a0056
krasch <github@krasch.io>
2018-02-01 14:28:05 +0100

    Save sample adversarials

--- create_adversarial.py
+++ create_adversarial.py
@@ -1,11 +1,13 @@
 import json
+import pathlib
 
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten
 from keras.models import load_model
 from keras import backend as K
 import numpy as np
-# import scipy.misc
+from PIL import Image
+
 
 from data import load_mnist, denormalise
 from util import JSONLogger
@@ -33,7 +35,7 @@ def init_mnist_model(pretrained=False):
         model.summary()
 
         (X_train, y_train), (X_test, y_test) = load_mnist()
-        model.fit(X_train, y_train, batch_size=64, epochs=20,
+        model.fit(X_train, y_train, batch_size=64, epochs=1,
                   validation_data=(X_test, y_test),
                   callbacks=[JSONLogger()], verbose=0)
 
@@ -45,12 +47,20 @@ class FGSM:
         self.model = model
         self.gradient_functions = {}
 
-    def generate_adversarial(self, original_image, target_class, epsilon):
+    def targeted_adversarial(self, original_image, target_class, epsilon):
         if target_class not in self.gradient_functions:
             self.gradient_functions[target_class] = self._init_gradient_function_(target_class)
 
         pertubation = np.sign(self.gradient_functions[target_class](original_image))
         adversarial = original_image + epsilon * pertubation
+        return np.clip(adversarial, pixel_min, pixel_max)
+
+    def untargeted_adversarial(self, original_image, original_class, epsilon):
+        if original_class not in self.gradient_functions:
+            self.gradient_functions[original_class] = self._init_gradient_function_(original_class)
+
+        pertubation = np.sign(self.gradient_functions[original_class](original_image))
+        adversarial = original_image - epsilon * pertubation
         return np.clip(adversarial, pixel_min, pixel_max)
 
     def _init_gradient_function_(self, target_class):
@@ -74,10 +84,9 @@ def test_model_strength(model, attack, test_images, test_labels, epsilon):
     outcome = []
 
     for img, label in zip(test_images, test_labels):
-        for target_label in set(range(10)) - set(label):
-            adversarial = attack.generate_adversarial(img, target_label, epsilon)
-            new_label, _ = classify(model, adversarial)
-            outcome.append(new_label == target_label)
+        adversarial = attack.targeted_adversarial(img, label, epsilon)
+        adversarial_label, _ = classify(model, adversarial)
+        outcome.append(label != adversarial_label)
 
     print(json.dumps({"epsilon": epsilon, "success_rate": np.array(outcome).mean()}))
 
@@ -87,13 +96,20 @@ def main():
     model = init_mnist_model(pretrained=False)
     attack = FGSM(model)
 
+
     _, (X_test, y_test) = load_mnist()
+    """
     for epsilon in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
         test_model_strength(model, attack, X_test[0:100], y_test[0:100], epsilon=epsilon)
+    """
 
-    #for i in range(10):
-    #    adversarial = attack.generate_adversarial(X_test[i], target_class=4, epsilon=0.3)
-    #    scipy.misc.imsave("generated/adversarial{}.jpg".format(i), denormalise(adversarial))
+    pathlib.Path('adversarials').mkdir(parents=True, exist_ok=True)
+    for i in range(10):
+        adversarial = attack.untargeted_adversarial(X_test[i], y_test[i].argmax(), epsilon=0.3)
+        adversarial_label, _ = classify(model, adversarial)
+
+        adversarial = denormalise(adversarial)
+        Image.fromarray(adversarial).save("adversarials/test{}_classified_as_{}.jpg".format(i, adversarial_label))
 
 main()
 
